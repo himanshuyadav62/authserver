394ccc55bb50b8c877b0852612a6f343eb500c0e
Himanshu yadav <himanshuyadav6224@gmail.com>
2024-09-14 12:33:56 +0530

    authserver

---
+++ src/test/java/com/exmaple/authserver/AuthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exmaple.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
